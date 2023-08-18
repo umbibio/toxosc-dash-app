1cb72f0db292f8629a6db357bdc3dc9a8bee2857
Argenis Arriojas Maldonado <arriojasmaldonado001@umb.edu>
2023-08-18 17:58:18 -0400

    Replaced circos plot with a Gantt graph

--- dash-app/pages/p05_peak_expression/callbacks.py
+++ dash-app/pages/p05_peak_expression/callbacks.py
@@ -12,12 +12,12 @@ from plotly.colors import sample_colorscale, get_colorscale
 
 from app import app, db
 
-from .layouts import scRNA_config, scATAC_config, available_periodic_genes
+from .layouts import scRNA_config, scATAC_config, available_periodic_genes, example_list_of_genes
 
 
-@app.callback(
-    Output('peak-expression-circos-plot', 'tracks'),
-    Input('peak-expression-gene-dropdown', 'value') )
+# @app.callback(
+#     Output('peak-expression-circos-plot', 'tracks'),
+#     Input('peak-expression-gene-dropdown', 'value') )
 def update_circos_plot(selected_genes):
     if not selected_genes:
         return []
@@ -45,12 +45,56 @@ def update_circos_plot(selected_genes):
 
 
 @app.callback(
+    Output('peak-expression-scatter-plot', 'figure'),
+    Input('peak-expression-gene-dropdown', 'value') )
+def update_graph(selected_genes):
+    if not selected_genes:
+        fig = go.Figure(layout={"xaxis": { "visible": False }, "yaxis": { "visible": False }})
+        fig.update_layout({"annotations": [
+            {
+                "text": "Select genes to visualize their expression peak times.",
+                "xref": "paper",
+                "yref": "paper",
+                "showarrow": False,
+                "font": {
+                    "size": 16
+                }
+            }
+        ]})
+
+    else:
+        df = db.select('scATAC_scRNA', 'peak_expression_chromosome_location', where={'GeneID': selected_genes})
+        df = df.rename(columns={'peak.ord.atac': 'ATAC', 'peak.ord.rna': 'RNA', 'dclass': 'Data Class'})
+        dff = df.melt(id_vars=['GeneID'], value_vars=['ATAC', 'RNA'], var_name='Data Class', value_name='time')
+
+        fig = px.bar(dff, base='time', x=[.1] * len(dff), y='GeneID', color='Data Class', orientation='h', barmode='group', )
+        fig.update_traces(hovertemplate="<br>".join([ "GeneID: %{y}", "peak time: %{base:.2f} hr", ]))
+        fig.layout.xaxis.range = [0, 6.2]
+        fig.layout.xaxis.title = 'peak time (hr)'
+        fig.update_layout({"height": max(500, 200 + 7 * len(dff))})
+
+    return fig
+
+
+@app.callback(
     Output('peak-expression-gene-dropdown', 'value'),
     Output('peak-expression-gene-loader-modal-text-area', 'value'),
     Input("peak-expression-gene-loader-modal-close-button", "n_clicks"),
+    Input("gene-list-clear-button", "n_clicks"),
+    Input("peak-expression-gene-example-button", "n_clicks"),
     State('peak-expression-gene-loader-modal-text-area', 'value'),
     State('peak-expression-gene-dropdown', 'value'), )
-def load_list_of_genes(n_clicks, textarea_content, selected_genes):
+def load_list_of_genes(n_clicks_modal, n_clicks_clear, n_clicks_example, textarea_content, selected_genes):
+
+    trigger = ctx.triggered[0]['prop_id'].split('.')[0]
+    match trigger:
+        case 'gene-list-clear-button':
+            return [], None
+        case 'peak-expression-gene-example-button':
+            return example_list_of_genes, None
+        case _:
+            pass
+
     if not selected_genes:
         selected_genes = []
     if not textarea_content:
--- dash-app/pages/p05_peak_expression/layouts.py
+++ dash-app/pages/p05_peak_expression/layouts.py
@@ -100,33 +100,108 @@ menu = [
         dcc.Dropdown(id='peak-expression-gene-dropdown', options=[
             {'label': gene, 'value': gene}
             for gene in available_periodic_genes
-        ], placeholder='Search...', multi=True),    
+        ], placeholder='Search...', multi=True, style={ "overflow-y":"scroll", "height": "200px"}),
     ])], class_name='mb-4 mt-4'),
-    dbc.Row([dbc.Col([
-        dbc.Button("Load List of Genes", id='peak-expression-gene-loader-button', size='sm', color='primary', outline=True),
-        dbc.Modal(
-            [
-                dbc.ModalHeader(dbc.ModalTitle("Load list of genes")),
-                dbc.ModalBody([
-                    dbc.Textarea(
-                        id='peak-expression-gene-loader-modal-text-area',
-                        rows=15,
-                        size="sm",
-                        className="mb-3",
-                        placeholder="Paste a list of genes. Valid separators are spaces, commas, semicolons and line breaks.",
-                    ),
-                ]),
-                dbc.ModalFooter(
-                    dbc.Button(
-                        "Load", id="peak-expression-gene-loader-modal-close-button", className="ms-auto", n_clicks=0
-                    )
+    dbc.Row([
+        dbc.Col([ dbc.Button("Load List of Genes", id='peak-expression-gene-loader-button', size='sm', color='primary', outline=True),]),
+        dbc.Col([ dbc.Button("Clear", id='gene-list-clear-button', size='sm', color='danger', outline=True),], class_name='text-end'),
+    ]),
+    dbc.Modal(
+        [
+            dbc.ModalHeader(dbc.ModalTitle("Load list of genes")),
+            dbc.ModalBody([
+                dbc.Textarea(
+                    id='peak-expression-gene-loader-modal-text-area',
+                    rows=15,
+                    size="sm",
+                    className="mb-3",
+                    placeholder="Paste a list of genes. Valid separators are spaces, commas, semicolons and line breaks.",
                 ),
-            ],
-            id="peak-expression-gene-loader-modal",
-            is_open=False,
-        ),
-    ])]),
+            ]),
+            dbc.ModalFooter(
+                dbc.Button(
+                    "Load", id="peak-expression-gene-loader-modal-close-button", className="ms-auto", n_clicks=0
+                )
+            ),
+        ],
+        id="peak-expression-gene-loader-modal",
+        is_open=False,
+    ),
 ]
 
-body = [circos_plot]
+body = [
+    dbc.Spinner([
+        dcc.Graph(id='peak-expression-scatter-plot'),
+    ], id=f'loading-peak-expression-scatter-plot', type='border', fullscreen=False, color='primary', delay_hide=100,),
+]
 
+example_list_of_genes = [
+    'TGME49_235370',
+    'TGME49_270200',
+    'TGME49_226680',
+    'TGME49_206710',
+    'TGME49_226040',
+    'TGME49_250690',
+    'TGME49_243250',
+    'TGME49_289880',
+    'TGME49_281650',
+    'TGME49_239800',
+    'TGME49_315510',
+    'TGME49_271270',
+    'TGME49_313780',
+    'TGME49_211280',
+    'TGME49_298010',
+    'TGME49_249440',
+    'TGME49_251740',
+    'TGME49_272640',
+    'TGME49_235690',
+    'TGME49_312580',
+    'TGME49_257470',
+    'TGME49_315220',
+    'TGME49_314358',
+    'TGME49_238420',
+    'TGME49_268176',
+    'TGME49_320030',
+    'TGME49_203985',
+    'TGME49_266630',
+    'TGME49_254290',
+    'TGME49_234260',
+    'TGME49_254870',
+    'TGME49_222100',
+    'TGME49_221250',
+    'TGME49_265080',
+    'TGME49_209200',
+    'TGME49_295125',
+    'TGME49_293520',
+    'TGME49_305590',
+    'TGME49_204880',
+    'TGME49_294560',
+    'TGME49_245550',
+    'TGME49_237190',
+    'TGME49_268182',
+    'TGME49_307020',
+    'TGME49_276130',
+    'TGME49_208910',
+    'TGME49_312100',
+    'TGME49_316290',
+    'TGME49_310010',
+    'TGME49_293440',
+    'TGME49_224540',
+    'TGME49_260580',
+    'TGME49_314250',
+    'TGME49_245475',
+    'TGME49_209590',
+    'TGME49_312300',
+    'TGME49_282210',
+    'TGME49_306350',
+    'TGME49_231210',
+    'TGME49_309150',
+    'TGME49_245640',
+    'TGME49_292300',
+    'TGME49_272030',
+    'TGME49_257370',
+    'TGME49_239020',
+    'TGME49_264420',
+    'TGME49_201730',
+    'TGME49_205662',
+]
