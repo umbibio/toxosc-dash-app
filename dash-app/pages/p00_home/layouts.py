14ee3b7b99a2ec582acad66541cb7473e127b261
Argenis Arriojas <ArriojasMaldonado001@umb.edu>
2023-10-07 19:54:05 -0400

    Update home page and references

--- dash-app/pages/p00_home/layouts.py
+++ dash-app/pages/p00_home/layouts.py
@@ -7,49 +7,41 @@ from references import cite
 
 
 description = f'''
-'''
-# Welcome!
-# This portal is related to {cite('name2023', markdown=True)}
-# '''
+# Toxo Single Cell Atlas
 
-contact = '''
+This web portal offers convenient access to some of the datasets and analysis tools used in the corresponding publication {cite('lou2023singlecell_preprint', markdown=True)}.
+The datasets here presented are scRNAseq and scATACseq on rapidly dividing Toxoplasma gondii tachyzoites. We also offer a visualization of the protein terciary
+structure of several transciption factors of the AP2 family.
+
+## Data and Code availablility
+
+Part of the data is available for download in the [Data](/toxosc/data) section of this portal.
+
+The analysis R code is available on GitHub: 
+[https://github.com/umbibio/scToxoplasmaCDC](https://github.com/umbibio/scToxoplasmaCDC) (DOI: [10.5281/zenodo.8219739](10.5281/zenodo.8219739)).
+scRNA-seq and CUT&RUN data (fastq) have been deposited to the Sequence Read Archive (SRA) under the accession number SUB13707798.
+
+## Contact
+
 For questions or comments please contact:
-
 [ArriojasMaldonado001@umb.edu](mailto:ArriojasMaldonado001@umb.edu?Subject=About%20your%20ToxoSC%20web%20app)
-'''
 
+## Citation
+{cite('lou2023singlecell_preprint', full=True, markdown=True)}
+'''
 
 menu = None
 
 body = [
     dbc.Row(dbc.Col(
         dbc.Card([
-            dbc.CardHeader(html.H2("Toxo Single Cell Atlas")),
             dbc.CardBody(
                 dbc.Row([
                     dbc.Col(dcc.Markdown(description),
-                    width={'size': '6', 'offset': '0'}),
-                # dbc.Col(
-                #     dbc.Carousel(items=[
-                #         {"key": "1", "src": "/appname/assets/carousel/1.png"},
-                #         {"key": "2", "src": "/appname/assets/carousel/2.png"},
-                #         {"key": "3", "src": "/appname/assets/carousel/3.png"},
-                #     ], interval=5000, className="carousel-fade", indicators=False)),
+                    width={'size': '12', 'offset': '0'}),
                 ])
             ),
-        ],),
+        ], color="light", outline=True),
     ), class_name="mb-4 mt-4"),
-    dbc.Row([
-        dbc.Col(dbc.Card([
-            dbc.CardHeader(html.H4("Citation")),
-            dbc.CardBody([
-                cite('lou2023mpm', full=True)
-            ]),
-        ],),),
-        dbc.Col(dbc.Card([
-            dbc.CardHeader(html.H4("Contact")),
-            dbc.CardBody(dcc.Markdown(contact)),
-        ],),),
-    ]),
 ]
 
--- dash-app/references.py
+++ dash-app/references.py
@@ -13,18 +13,18 @@ for item in references:
 
 
 months = {
-    '1': 'January', '01': 'January', 'Jan': 'January',
-    '2': 'February', '02': 'February', 'Feb': 'February',
-    '3': 'March', '03': 'March', 'Mar': 'March',
-    '4': 'April', '04': 'April', 'Apr': 'April',
-    '5': 'May', '05': 'May', 'May': 'May',
-    '6': 'June', '06': 'June', 'Jun': 'June',
-    '7': 'July', '07': 'July', 'Jul': 'July',
-    '8': 'August', '08': 'August', 'Aug': 'August',
-    '9': 'September', '09': 'September', 'Sep': 'September',
-    '10': 'October', 'Oct': 'October',
-    '11': 'November', 'Nov': 'November',
-    '12': 'December', 'Dec': 'December',
+    '1': 'January', '01': 'January', 'Jan': 'January', 'January': 'January',
+    '2': 'February', '02': 'February', 'Feb': 'February', 'February': 'February',
+    '3': 'March', '03': 'March', 'Mar': 'March', 'March': 'March',
+    '4': 'April', '04': 'April', 'Apr': 'April', 'April': 'April',
+    '5': 'May', '05': 'May', 'May': 'May', 'May': 'May',
+    '6': 'June', '06': 'June', 'Jun': 'June', 'June': 'June',
+    '7': 'July', '07': 'July', 'Jul': 'July', 'July': 'July',
+    '8': 'August', '08': 'August', 'Aug': 'August', 'August': 'August',
+    '9': 'September', '09': 'September', 'Sep': 'September', 'September': 'September',
+    '10': 'October', 'Oct': 'October', 'October': 'October',
+    '11': 'November', 'Nov': 'November', 'November': 'November',
+    '12': 'December', 'Dec': 'December', 'December': 'December',
 }
 
 
@@ -55,22 +55,38 @@ def cite(key, full=False, markdown=False):
 
     if 'doi' in ref:
         doi_url = 'https://doi.org/' + ref['doi']
-        doi_url = html.A(doi_url, href=doi_url, target='blank')
+        if markdown:
+            doi_url = f"[{doi_url}]({doi_url})"
+        else:
+            doi_url = html.A(doi_url, href=doi_url, target='blank')
     else:
         doi_url = None
 
-    if ref['journal']:
+    if ref.get('journal'):
         published_str = "Published:"
+        jour = ref['journal']
     else:
         published_str = "Submitted:"
+        jour = ''
+        if ref.get('publisher'):
+            jour = ref['publisher']
     if 'month' in ref:
         published_str += ' ' + months[ref['month']] 
     published_str += ' ' + ref['year']
 
+    if markdown:
+        return f'''
+“{ref['title'].strip('{}')}.”  
+{authors_str}.  
+*{jour}*{', Volume ' + ref['volume'] if 'volume' in ref else ''}{', Issue ' + ref['number'] if 'number' in ref else ''}  
+{doi_url}  
+{published_str}
+'''
+
     return html.Div([
         html.Span('“' + ref['title'].strip('{}') + '”. '), html.Br(),
         html.Span(authors_str + '. '), html.Br(),
-        html.I(ref['journal']),
+        html.I(jour),
         ', Volume ' + ref['volume'] if 'volume' in ref else None,
         ', Issue ' + ref['number'] if 'number' in ref else None,
         html.Br() if doi_url else None,
